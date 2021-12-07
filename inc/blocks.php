6acd86782da89395e2da8bd2c1a08b6dfe34da07
Boone B Gorges <boonebgorges@gmail.com>
2021-12-07 16:17:09 -0600

    Define Article Blade block pattern.

--- inc/blocks.php
+++ inc/blocks.php
@@ -33,6 +33,13 @@ add_action( 'enqueue_block_editor_assets', 'ramp_register_block_assets' );
  * strings in default templates.
  */
 function ramp_register_block_patterns() {
+	register_block_pattern_category(
+		'ramp-theme/content-blades',
+		[
+			'label' => __( 'Content Blades', 'ramp' ),
+		]
+
+	);
 	register_block_pattern(
 		'ramp-theme/research-topics-heading',
 		[
@@ -56,6 +63,34 @@ function ramp_register_block_patterns() {
 							) .
 							'<!-- /wp:paragraph -->',
 			'inserter'   => false,
+		]
+	);
+
+	register_block_pattern(
+		'ramp-theme/articles-blade',
+		[
+			'title'       => __( 'Articles Blade', 'ramp-theme' ),
+			'description' => __( 'A two-column preview of recent and featured articles, useful for the home page.', 'ramp' ),
+			'content'     => sprintf( '<!-- wp:group {"tagName":"section"} -->
+<section class="wp-block-group item-type-block"><!-- wp:group {"tagName":"header"} -->
+<header class="wp-block-group item-type-block-header">
+<!-- wp:heading --><h2 id="articles-blade">%s</h2><!-- /wp:heading -->
+
+<!-- wp:paragraph --><p class="item-type-block-all-link"><a href="%s">%s</a></p><!-- /wp:paragraph -->
+
+<!-- wp:pattern {"slug":"ramp-theme/articles-all-link"} /--></header>
+
+<!-- /wp:group -->
+
+<!-- wp:ramp/article-teasers /--></section>
+
+<!-- /wp:group -->',
+								esc_html__( 'Articles', 'ramp-theme' ),
+								esc_url( get_post_type_archive_link( 'ssrc_expref_pt' ) ),
+								esc_html__( 'More Articles', 'ramp-theme' )
+							),
+			'inserter'    => true,
+			'categories'  => [ 'ramp-theme/content-blades' ],
 		]
 	);
 }
