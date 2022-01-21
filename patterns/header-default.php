8601b20d237a0bc81dc63f350684965364e2bd26
Boone B Gorges <boonebgorges@gmail.com>
2022-01-21 11:20:36 -0600

    Secondary nav style adjustments.

--- patterns/header-default.php
+++ patterns/header-default.php
@@ -2,23 +2,21 @@
 /**
  * Default header block pattern
  */
-/*
-'<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"28px"}}},"className":"header-inner","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"noWrap"}} -->' .
-'<!-- wp:group {"className":"header-inner","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"noWrap"}} -->' .
-*/
 $header_props = array(
 	'title'      => __( 'Default header', 'ramp-theme' ),
 	'categories' => [ 'ramp-theme/headers' ],
 	'blockTypes' => array( 'core/template-part/header' ),
 	'content'    => sprintf(
+						'<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"8px","right":"0px","bottom":"8px","left":"0px"}}},"backgroundColor":"almost-black","className":"secondary-nav has-white-color has-text-color","layout":{"inherit":true}} -->' .
+						'<div class="wp-block-group secondary-nav has-white-color has-almost-black-background-color has-text-color has-background has-link-color" style="padding-top:8px;padding-right:0px;padding-bottom:8px;padding-left:0px">' .
+							'<!-- wp:navigation {"ref":%d,"layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} -->' .
+							'%s' .
+							'<!-- /wp:navigation -->' .
+						'</div>' .
+						'<!-- /wp:group -->' .
+
 						'<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"28px"}}},"className":"header-default","backgroundColor":"light-grey","layout":{"inherit":true}} -->' .
 						'<div class="wp-block-group header-default has-light-grey-background-color has-background" style="padding-bottom:28px;">' .
-							'<!-- wp:navigation-area {"area":"secondary","className":"secondary-nav"} -->' .
-								'<!-- wp:navigation {"ref":%d,"backgroundColor":"cyan-bluish-gray","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"}} -->' .
-								'%s' .
-								'<!-- /wp:navigation -->' .
-							'<!-- /wp:navigation-area -->' .
-
 							'<!-- wp:group {"className":"header-inner","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"noWrap"}} -->' .
 							'<div class="wp-block-group header-inner">' .
 								'<!-- wp:group {"layout":{"wideSize":"300px","contentSize":"300px"}} -->' .
@@ -28,11 +26,9 @@ $header_props = array(
 								'</div>' .
 								'<!-- /wp:group -->' .
 
-								'<!-- wp:navigation-area {"area":"primary","className":"primary-nav"} -->' .
-									'<!-- wp:navigation {"ref":%d} -->' .
-									'%s' .
-									'<!-- /wp:navigation -->' .
-								'<!-- /wp:navigation-area -->' .
+								'<!-- wp:navigation {"ref":%d} -->' .
+								'%s' .
+								'<!-- /wp:navigation -->' .
 							'</div>' .
 							'<!-- /wp:group -->' .
 						'</div>' .
