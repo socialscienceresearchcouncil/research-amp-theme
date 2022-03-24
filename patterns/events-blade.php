72e7b32310d0b5ef3e43ddc2a7f206f828398603
Boone B Gorges <boonebgorges@gmail.com>
2022-03-24 14:53:59 -0500

    Update pattern for events teasers.

--- patterns/events-blade.php
+++ patterns/events-blade.php
@@ -2,16 +2,21 @@
 
 $pattern_content = sprintf(
 	'<!-- wp:group {"tagName":"section","layout":{"inherit":true}} -->
-	<section class="wp-block-group item-type-block"><!-- wp:group {"tagName":"header"} -->
-	<header class="wp-block-group item-type-block-header">
-	<!-- wp:heading --><h2 id="news-items-blade">%s</h2><!-- /wp:heading -->
+	<section class="wp-block-group item-type-block">
+		<!-- wp:group {"tagName":"header","style":{"spacing":{"padding":{"bottom":"32px"}}}} -->
+		<header class="wp-block-group item-type-block-header" style="padding-bottom:32px;">
+			<!-- wp:heading {"fontSize":"h4"} -->
+				<h2 class="has-h-4-font-size" id="news-items-blade-blade">%s</h2>
+			<!-- /wp:heading -->
 
-	<!-- wp:paragraph --><p class="item-type-block-all-link"><a href="%s">%s</a></p><!-- /wp:paragraph -->
+			<!-- wp:paragraph -->
+				<p class="item-type-block-all-link"><a href="%s">%s</a></p>
+			<!-- /wp:paragraph -->
+		</header>
+		<!-- /wp:group -->
 
-	</header><!-- /wp:group -->
-
-	<!-- wp:ramp/event-teasers {"researchTopic":"all"} /--></section>
-
+		<!-- wp:ramp/event-teasers {"contentMode":"all", "numberOfItems": 3} /-->
+	</section>
 	<!-- /wp:group -->',
 	esc_html__( 'Upcoming Events', 'ramp-theme' ),
 	esc_url( tribe_events_get_url() ),
