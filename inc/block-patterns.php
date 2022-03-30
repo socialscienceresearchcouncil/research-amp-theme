d842e880805c5ac4be1d5d6ebfeefd4b5a2864f2
Boone B Gorges <boonebgorges@gmail.com>
2022-03-30 16:46:52 -0500

    Eliminate research-topic-intro pattern.

--- inc/block-patterns.php
+++ inc/block-patterns.php
@@ -41,7 +41,6 @@ function ramp_theme_register_block_patterns() {
 		'news-items-archive-intro',
 		'profiles-archive-intro',
 		'research-reviews-archive-intro',
-		'research-topic-intro',
 		'research-topics-archive-intro',
 
 		// Misc
