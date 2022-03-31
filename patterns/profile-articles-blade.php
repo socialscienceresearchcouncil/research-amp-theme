a5dd804cab7af49a078de33ac0e9e61fd29ec9c0
Boone B Gorges <boonebgorges@gmail.com>
2022-03-31 14:52:40 -0500

    Articles blade on profile page.

--- patterns/profile-articles-blade.php
+++ patterns/profile-articles-blade.php
@@ -6,20 +6,20 @@ $pattern_content = sprintf(
 		<!-- wp:group {"tagName":"header","style":{"spacing":{"padding":{"bottom":"8px"}}}} -->
 		<header class="wp-block-group item-type-block-header" style="padding-bottom:8px;">
 			<!-- wp:heading	{"level":3,"fontSize":"h6"} -->
-				<h3 class="has-h-6-font-size" id="research-reviews-blade">%s</h3>
+				<h3 class="has-h-6-font-size" id="articles-blade">%s</h3>
 			<!-- /wp:heading -->
 		</header>
 		<!-- /wp:group -->
 
-		<!-- wp:ramp/research-review-teasers {"contentMode":"auto", "numberOfItems": 3, "order": "latest", "variationType":"list"} /-->
+		<!-- wp:ramp/article-teasers {"contentMode":"auto", "numberOfItems": 3, "order": "latest", "variationType":"list"} /-->
 	</section>
 	<!-- /wp:group -->',
-	esc_html__( 'Research Reviews', 'ramp-theme' )
+	esc_html__( 'Articles', 'ramp-theme' )
 );
 
 return [
-	'title'       => __( 'Research Review Blade for Profiles', 'ramp-theme' ),
-	'description' => __( 'A preview of recent Research Reviews associated with a specific Profile.', 'ramp' ),
+	'title'       => __( 'Articles Blade for Profiles', 'ramp-theme' ),
+	'description' => __( 'A preview of recent Articles associated with a specific Profile.', 'ramp' ),
 	'content'     => $pattern_content,
 	'inserter'    => true,
 	'categories'  => [ 'ramp-theme/content-blades' ],
