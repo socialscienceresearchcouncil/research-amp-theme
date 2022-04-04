4183f316e3722735f230eeeef880888a364da22a
Boone B Gorges <boonebgorges@gmail.com>
2022-04-04 11:24:46 -0500

    Basic support for the-event-calendar integration.
    * We override the-events-calendar and allow the WP template hierarchy to kick in, so that we can load our block templates
    * archive-tribe_events.html is a wrapper for a ramp/the-events-calendar block, which renders the PHP template from the-events-calendar
    * single-tribe_events.html moves some elements, such as buttons, event details, and profiles, out of the post_content
    
    Site Editor editing doesn't fully work - the-events-calendar doesn't fully
    load the necessary blocks when not looking at the tribe_events post type.

---
+++ patterns/events-archive-intro.php
@@ -0,0 +1,25 @@
+<?php
+
+$pattern_content = sprintf(
+	'<!-- wp:group {"layout":{"inherit":true},"className":"directory-intro"} -->
+	<div class="wp-block-group">
+		<!-- wp:heading -->
+		<h2>%s</h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph {"placeholder":"%s"} -->
+		<p></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->',
+	esc_html__( 'Events', 'ramp-theme' ),
+	esc_html__( 'Enter a description of the Events archive', 'ramp-theme' )
+);
+
+return [
+	'title'       => __( 'Events Archive Intro', 'ramp-theme' ),
+	'description' => __( 'The introductory section of the Events archive', 'ramp' ),
+	'content'     => $pattern_content,
+	'inserter'    => false,
+	'categories'  => [ 'ramp-theme' ],
+];
---
+++ tribe/events/blocks/parts/venue.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Venue template part for the Event Venue block
+ *
+ */
+
+if ( ! tribe_get_venue_id() ) {
+	return;
+}
+$attributes = $this->get( 'attributes', [] );
+
+$phone   = tribe_get_phone();
+
+$venue_id = tribe_get_venue_id();
+$url      = get_permalink( $venue_id );
+?>
+
+<div class="tribe-block__venue__meta">
+	<h3 class="tribe-events-single-section-title has-h-6-font-size"><?php esc_html_e( 'Venue', 'ramp' ); ?></h3>
+
+	<div class="tribe-block__venue__name">
+		<?php echo tribe_get_venue_link() ?>
+	</div>
+
+	<?php do_action( 'tribe_events_single_meta_venue_section_start' ) ?>
+
+	<?php if ( tribe_address_exists() ) : ?>
+		<address class="tribe-block__venue__address">
+			<?php echo tribe_get_full_address(); ?>
+
+			<?php if ( tribe_show_google_map_link() ) : ?>
+				<?php echo tribe_get_map_link_html(); ?>
+			<?php endif; ?>
+		</address>
+	<?php endif; ?>
+
+	<?php if ( ! empty( $phone ) ) : ?>
+		<span class="tribe-block__venue__phone"><?php echo $phone ?></span><br />
+	<?php endif ?>
+
+	<?php if ( ! empty( $url ) ) : ?>
+		<span class="tribe-block__venue__website"><a href="<?php echo esc_url( $url ); ?>"><?php esc_html_e( 'Visit Venue Website', 'ramp' ); ?></a></span><br />
+	<?php endif ?>
+
+	<?php do_action( 'tribe_events_single_meta_venue_section_end' ) ?>
+</div>
