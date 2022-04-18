5c37d071bb8b3f71e35a6fb5a2433c250e93d2d4
Boone B Gorges <boonebgorges@gmail.com>
2022-04-18 09:57:30 -0500

    Fix typo.

--- conditional-patterns/event-profiles-blade.php
+++ conditional-patterns/event-profiles-blade.php
@@ -14,7 +14,7 @@ printf(
 		<!-- wp:group {"tagName":"header","style":{"spacing":{"padding":{"bottom":"32px"}}}} -->
 		<header class="wp-block-group item-type-block-header" style="padding-bottom:32px;">
 			<!-- wp:heading {"level":3, "style":{"typography":{"textTransform":"uppercase"}},"fontSize":"h5"} -->
-				<h3 class="has-h-5-font-size" id="profiles-blade" style="text-tranform: uppercase;">%s</h3>
+				<h3 class="has-h-5-font-size" id="profiles-blade" style="text-transform: uppercase;">%s</h3>
 			<!-- /wp:heading -->
 		</header>
 		<!-- /wp:group -->
